a44043c752f58cec2b218126b8b8c5f4ee90c53a
Vicenc Garcia-Altes <vincentredrum@gmail.com>
2019-01-16 22:47:42 +0000

    Micro log

--- notes-app-client/src/index.js
+++ notes-app-client/src/index.js
@@ -7,6 +7,8 @@ import "./index.css";
 import Amplify from "aws-amplify";
 import config from "./config";
 
+console.log(process.env.REGION);
+
 Amplify.configure({
   Auth: {
     mandatorySignIn: true,
